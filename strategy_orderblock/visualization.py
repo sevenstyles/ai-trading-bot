ac80c5f18151e5510fc821e236a19a66b858a283
sevenstyles <sevenstylesinfo@gmail.com>
2025-02-08 13:06:46 +1100

    safe point - working

--- strategy_orderblock/visualization.py
+++ strategy_orderblock/visualization.py
@@ -18,7 +18,7 @@ def visualize_order_blocks(df, order_blocks, filename="order_blocks.png", mitiga
 
     # Create the figure and axes
     fig, ax1 = plt.subplots(figsize=(12, 6))
-    ax2 = ax1.twiny()  # Create a twin Axes sharing the y-axis
+    #ax2 = ax1.twiny()  # Create a twin Axes sharing the y-axis
 
     # Plot candlesticks on ax1
     width = 0.8 / len(df)  # Width of the candlesticks
@@ -32,7 +32,7 @@ def visualize_order_blocks(df, order_blocks, filename="order_blocks.png", mitiga
     # Create a sequence of numbers for the x-axis
     x = np.arange(len(df))
 
-    # Plot order block zones on ax2
+    # Plot order block zones on ax1
     for ob in order_blocks:
         mitigated = is_order_block_mitigated(ob, df, len(df) - 1, mitigation_type)
         if not mitigated:
@@ -40,27 +40,27 @@ def visualize_order_blocks(df, order_blocks, filename="order_blocks.png", mitiga
                 color = 'green'
                 # Debugging: Print order block details
                 print(f"Plotting BULLISH OB at index {ob.index}, bar_low={ob.bar_low}, bar_high={ob.bar_high}")
-                ax2.axvspan(x[ob.index], x[-1], facecolor=color, alpha=0.2, label='demand')
+                ax1.fill_betweenx([ob.bar_low, ob.bar_high], df['timestamp'][ob.index], df['timestamp'].iloc[-1], facecolor=color, alpha=0.2, label='demand')
 
             elif ob.bias == BEARISH:
                 color = 'red'
                 # Debugging: Print order block details
                 print(f"Plotting BEARISH OB at index {ob.index}, bar_low={ob.bar_low}, bar_high={ob.bar_high}")
-                ax2.axvspan(x[ob.index], x[-1], facecolor=color, alpha=0.2, label='supply')
+                ax1.fill_betweenx([ob.bar_low, ob.bar_high], df['timestamp'][ob.index], df['timestamp'].iloc[-1],  facecolor=color, alpha=0.2, label='supply')
 
     # Format the x-axis of ax1 to show dates
     ax1.xaxis.set_major_locator(mdates.AutoDateLocator())
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
     plt.setp(ax1.get_xticklabels(), rotation=45, ha="right")
 
-    # Set the x-axis limits of ax2 to the range of the data
-    ax2.set_xlim(x[0], x[-1])
+    ## Set the x-axis limits of ax2 to the range of the data
+    #ax2.set_xlim(x[0], x[-1])
 
-    # Hide the x-axis labels and ticks on ax2
-    ax2.xaxis.set_visible(False)
+    ## Hide the x-axis labels and ticks on ax2
+    #ax2.xaxis.set_visible(False)
 
-    # Align the y-axis limits of ax1 and ax2
-    ax2.set_ylim(ax1.get_ylim())
+    ## Align the y-axis limits of ax1 and ax2
+    #ax2.set_ylim(ax1.get_ylim())
 
     # Add labels and title
     ax1.set_xlabel('Date')
@@ -69,9 +69,9 @@ def visualize_order_blocks(df, order_blocks, filename="order_blocks.png", mitiga
 
     # Add legend (only show one label per type)
     handles1, labels1 = ax1.get_legend_handles_labels()
-    handles2, labels2 = ax2.get_legend_handles_labels()
-    handles = handles1 + handles2
-    labels = labels1 + labels2
+    #handles2, labels2 = ax2.get_legend_handles_labels()
+    handles = handles1 #+ handles2
+    labels = labels1 #+ labels2
     by_label = dict(zip(labels, handles))
     ax1.legend(by_label.values(), by_label.keys())
 
